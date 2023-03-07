98c7cd0753cb487bd3cb75e0cb18119caab0ba4c
yrosales <yoroslop@gmail.com>
2023-03-07 14:09:56 -0500

    cross origin

--- src/main/java/demo/DeviceControler.java
+++ src/main/java/demo/DeviceControler.java
@@ -2,6 +2,7 @@ package demo;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ class DeviceControler {
 	}
 	// end::get-aggregate-root[]
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping("/devices")
 	Device newDevice(@RequestBody Device newDevice) {
 		return deviceRepository.save(newDevice);
